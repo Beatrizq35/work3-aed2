95fe4d809284dec6f9bd0283ea1aa550a5a5142c
Ruthelene <ruthelene.farias@gmail.com>
2026-06-04 17:07:38 -0400

    att

--- IndiceArvBST.h
+++ IndiceArvBST.h
@@ -2,14 +2,14 @@
 #define INDICE_ARV_BST_H
 
 typedef struct NoBST {
-    int valor; // Pode ser a matrícula (chave) ou a idade
-    int rrn;   // Número do registro no arquivo (Relative Record Number)
+    float valor; // <-- MUDOU PARA FLOAT para aceitar o CR (ex: 8.5)
+    int rrn;   
     struct NoBST *esq, *dir;
 } NoBST;
 
-NoBST* inserirBST(NoBST* raiz, int valor, int rrn);
-int buscarBST(NoBST* raiz, int valor); // Retorna o RRN
-void emOrdemBST(NoBST* raiz); // Para testes pequenos
-void buscarIntervaloBST(NoBST* raiz, int min, int max, int* resultados, int* qtd); // Para >, <
+NoBST* inserirBST(NoBST* raiz, float valor, int rrn);
+int buscarBST(NoBST* raiz, float valor); 
+void emOrdemBST(NoBST* raiz); 
+void buscarIntervaloBST(NoBST* raiz, float min, float max, int* resultados, int* qtd); 
 
 #endif
