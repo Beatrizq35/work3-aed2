84811ebee8f3123549dcdaeac67990b06bce5ac0
Ruthelene <ruthelene.farias@gmail.com>
2026-06-04 17:09:24 -0400

    Update Registro.h

--- Registro.h
+++ Registro.h
@@ -1,12 +1,10 @@
 #ifndef REGISTRO_H
 #define REGISTRO_H
 
-#include <stdio.h>
-
-// Tamanho aproximado: 4 + 4 + 50 + 30 + 40 = 128 bytes
 typedef struct {
-    int matricula;      // Atributo Chave
-    int idade;          // Atributo não-chave (para consultas de intervalo)
+    int matricula;      // Chave
+    int idade;          // Atributo informativo
+    float cr;           // Atributo Não-Chave (novo campo)
     char nome[50];
     char curso[30];
     char email[40];
